82585a0f0c55df0aa106a73295d5c85cb5aca7aa
Michael Ngo <mn542@cornell.edu>
2023-10-30 17:02:15 -0400

    Add empty.env

--- src/main.py
+++ src/main.py
@@ -28,14 +28,14 @@ def load_config(path):
     return config
 
 
-def main(video_path:str, results_out:str= "tmp/results.txt") -> None:
+def main(video_path: str, results_out: str = "tmp/results.txt") -> None:
     """
     Sequentially initialises and runs model running and processing tasks.
-    Input: 
+    Input:
         video_path: relative path to video to process
         results_out: relative path to write results
     Side Effect:
-        Writes to results [results_out] 
+        Writes to results [results_out]
     """
     config = load_config("config.yaml")
     model_vars = config["model_vars"]
@@ -56,12 +56,16 @@ def main(video_path:str, results_out:str= "tmp/results.txt") -> None:
 
     processrunner.run()
     results = processrunner.get_results()
-    with open(results_out,'w') as file:
+    with open(results_out, "w") as file:
         file.write(results)
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
+        print("got here")
         main(sys.argv[1])
     else:
-        main("data/training_data.mp4")# Pass the first command-line argument to the main function
+        print("versus got here")
+        main(
+            "data/training_data.mp4"
+        )  # Pass the first command-line argument to the main function
