9af676255bffc8ebba0740c7ed305e274c77c96c
Michael Ngo <mn542@cornell.edu>
2023-10-29 21:26:07 -0400

    Intro temp into gitignore - player filter thresholding gentle for short videos

--- src/processrunner.py
+++ src/processrunner.py
@@ -35,7 +35,8 @@ class ProcessRunner:
 
     def run_possession(self):
         self.state.recompute_possesssions()
-        self.state.filter_players(threshold=100)
+        threshold = max(100, len(self.state.frames) / 3)  # in case of short video
+        self.state.filter_players(threshold=threshold)
         self.state.recompute_possession_list(threshold=10, join_threshold=20)
         self.state.recompute_pass_from_possession()
 
