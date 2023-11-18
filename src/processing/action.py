26a03210060db43dcf895e057bb4d33dd45a7b70
Benson Zhang <bjz22@cornell.edu>
2023-11-17 21:41:21 -0500

    config

--- src/processing/action.py
+++ src/processing/action.py
@@ -1,16 +1,19 @@
 import torch
 from state import GameState, Frame, PlayerFrame, Keypoint
 from pose_estimation.pose_estimate import KeyPointNames, AngleNames
+from args import DARGS
 
 COMBINATIONS = AngleNames.combinations
 ANGLE_NAMES = AngleNames.list
 
 class ActionRecognition:
-    def __init__(self, state: GameState):
+    def __init__(self, state: GameState, args=DARGS) -> None:
         self.state = state
+        self.THRESHOLD = args['shot_threshold']
+        self.ANGLE_THRESHOLD = args['angle_threshold']
 
     @staticmethod
-    def is_shot(player_frame: PlayerFrame):
+    def is_shot(player_frame: PlayerFrame, THRESHOLD, ANGLE_THRESHOLD):
         """
         Takes in keypoint and angle data for a player in a frame and returns whether
         or not the player is shooting. Currently uses a simple threshold heuristic.
@@ -26,8 +29,6 @@ class ActionRecognition:
         left_elbow = player_frame.angles('left_elbow')
         right_elbow = player_frame.angles('right_elbow')
 
-        THRESHOLD = 0.7 # TODO abstract into config file
-        ANGLE_THRESHOLD = 150
         curr = 0
         if left_wrist[1] < left_shoulder[1] and right_wrist[1] < right_shoulder[1]:
             curr += 0.6
@@ -40,6 +41,6 @@ class ActionRecognition:
     def shot_detect(self):
         for frame in self.state.frames:  # type: Frame
             for player_id, player_frame in frame.players.items():  # type: PlayerFrame
-                if self.is_shot(player_frame):
+                if self.is_shot(player_frame, self.THRESHOLD, self.ANGLE_THRESHOLD):
                     shot_interval = (frame.frameno, player_id)
                     self.state.shots.append(shot_interval)
