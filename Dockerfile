9ce43ce090c833b47379fe93abb45af7f551698d
Kenneth Chiem <kc696@cornell.edu>
2024-07-25 00:49:25 -0500

    add install dependencies for cv2 in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,8 @@ RUN apt-get update && apt-get install -y \
   gcc \
   && apt-get clean
 
+RUN apt-get install ffmpeg libsm6 libxext6  -y
+
 RUN pip install -r requirements.txt
 
 CMD uvicorn src.api.backend:app --reload --host 0.0.0.0 --port 8000
--- src/view/app.py
+++ src/view/app.py
@@ -30,7 +30,7 @@ if "state" not in st.session_state:
     st.session_state.user_file = "tmp/user_upload.mp4"
 
 # Backend Connection
-SERVER_URL = "http://127.0.0.1:8000/"
+SERVER_URL = "http://34.205.37.69:8000/"
 
 
 def process_video(video_file):
